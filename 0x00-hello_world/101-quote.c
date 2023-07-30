2bf9d1212364bd3b45e21d826edb4b3f610052f5
StellarGitScholar <dumbocollenzo@gmail.com>
2023-07-30 04:34:54 -0700

    I created the file 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - a simple c program to print to stdout
+ *
+ * write: command to print given input to stdout
+ *
+ * Return: 1
+ */
+
+int main(void)
+{
+	write(1, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59);
+}
