818152e5bd8d7b6239e4f847a0c54d4808bfa5a4
StellarGitScholar <dumbocollenzo@gmail.com>
2023-07-30 04:46:15 -0700

    I made changes to the file 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - a simple c program to print to stdout
@@ -10,5 +11,7 @@
 
 int main(void)
 {
-	write(1, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
+	return(1);
 }
