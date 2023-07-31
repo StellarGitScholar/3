8936096eaf41e55a740f9d9298526b9fbad84381
StellarGitScholar <dumbocollenzo@gmail.com>
2023-07-31 16:36:42 -0700

    I created the file 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - function to print the alphabet ten times
+ */
+
+void print_alphabet_x10(void)
+{
+	char a, b;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		for (b = 'a'; b <= 'z'; b++)
+			_putchar(b);
+
+		_putchar('\n');
+	}
+}
