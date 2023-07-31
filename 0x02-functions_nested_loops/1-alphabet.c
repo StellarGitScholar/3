63cf7620df1444d2ab0e6abcd0966fc65923b55c
StellarGitScholar <dumbocollenzo@gmail.com>
2023-07-31 15:58:49 -0700

    I created the file 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - simple c function to print lowercase alphabets
+ */
+
+void print_alphabet(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+		_putchar(a);
+
+	_putchar('\n');
+}
