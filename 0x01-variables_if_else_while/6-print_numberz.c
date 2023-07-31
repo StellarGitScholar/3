41fe733d3a88055da737ce06de9486bcfdd7c8a9
StellarGitScholar <dumbocollenzo@gmail.com>
2023-07-31 05:15:22 -0700

    I created the file 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - c program to print the single digit numbers of base 10
+ *
+ * n: variable to store and print the numbers
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9';  ++)
+		putchar(n);
+
+	putchar('\n');
+
+	return (0);
+}
