1491cb1d378733729b7dac086fe49fe8dd3448dc
StellarGitScholar <dumbocollenzo@gmail.com>
2023-07-31 08:46:27 -0700

    I created the file 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - simple c program to print number combos
+ *
+ * m: variable to store and print the numbers
+ *
+ * n: variable to store and print the numbers
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int m, n;
+
+	for (m = '0'; m <= '9')
+	{
+		for (n = '0'; n <= '9')
+		{
+			if (m != n && m < n)
+			{
+				putchar(m);
+				putchar(n);
+
+				if (m != '8' || n != '9')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
