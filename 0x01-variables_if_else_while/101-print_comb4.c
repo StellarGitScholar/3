48c502ffdf8097868e2940db0d55902013290940
StellarGitScholar <dumbocollenzo@gmail.com>
2023-07-31 10:32:57 -0700

    I created the file 101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - simple c program to print number combos
+ *
+ * m, n, o: variables to store and print numbers
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int m, n, o;
+
+	for (m = '0'; m <= '9'; m++)
+	{
+		for (n = '0'; n <= '9'; n++)
+		{
+			for (o = n + 1; o <= '9'; o++)
+			{
+				putchar(m);
+				putchar(n);
+				putchar(o);
+
+				if (m != '7' || n != '8' || o != '9')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
