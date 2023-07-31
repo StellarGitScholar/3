8396d307f86eedd36b921783eaf589b749594253
StellarGitScholar <dumbocollenzo@gmail.com>
2023-07-31 11:11:44 -0700

    I created the file 102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - simple c program to print number combos
+ *
+ * m, n: variab,es to store and print the numbers
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int m, n;
+
+	for (m = '0'; m <= '99'; m++)
+	{
+		for (n = m + 1; n <= '99'; n++)
+		{
+			putchar((m / 10) + '0');
+			putchar((m % 10) + '0');
+			putchar(' ');
+			putchar((n / 10) + '0');
+			putchar((n % 10) + '0');
+
+			if (m != '98' || n != '99')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
