2930d2c49515db2c58408616ffa408b9cf5d313e
StellarGitScholar <dumbocollenzo@gmail.com>
2023-08-01 06:34:21 -0700

    I created the file 9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ *times_table - c function that prints the 9 times table
+ */
+
+void times_table(void)
+{
+	int m, n, o;
+
+	for (m = 0; m <= 9; m++)
+	{
+		for (n = 0; n <= 9; n++)
+		{
+			o = m * n;
+
+			if (o <= 9)
+			{
+				_putchar(' ');
+			} else
+			{
+				_putchar((o / 10) + '0');
+			}
+
+			_putchar((o % 10) + '0';
+
+			if (n != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+
+		_putchar('\n');
+	}
+}
